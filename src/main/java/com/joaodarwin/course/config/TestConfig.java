4d9a325b39f8c254eec033c256ed48ba88208311
João Darwin <joaodarwin.ip22@gmail.com>
2022-11-29 10:22:18 -0300

    Implementação das camadas Category

--- src/main/java/com/joaodarwin/course/config/TestConfig.java
+++ src/main/java/com/joaodarwin/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.joaodarwin.course.entities.Category;
 import com.joaodarwin.course.entities.Order;
 import com.joaodarwin.course.entities.User;
 import com.joaodarwin.course.entities.enums.OrderStatus;
+import com.joaodarwin.course.repositories.CategoryRepository;
 import com.joaodarwin.course.repositories.OrderRepository;
 import com.joaodarwin.course.repositories.UserRepository;
 
@@ -23,9 +25,17 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Eletronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computer");
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
@@ -34,6 +44,7 @@ public class TestConfig implements CommandLineRunner{
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		Order o4 = new Order(null, Instant.parse("2022-11-20T12:04:50Z"), OrderStatus.CANCELED, u2);
 		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
 	}
