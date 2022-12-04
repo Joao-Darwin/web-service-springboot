ac0888a29591f705e915eee61f0b5fc360d695af
João Darwin <joaodarwin.ip22@gmail.com>
2022-12-04 11:02:51 -0300

    Entidade OrderItem, associação muitos-para-muitos entre Order e OrderItem

--- src/main/java/com/joaodarwin/course/config/TestConfig.java
+++ src/main/java/com/joaodarwin/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.joaodarwin.course.entities.Category;
 import com.joaodarwin.course.entities.Order;
+import com.joaodarwin.course.entities.OrderItem;
 import com.joaodarwin.course.entities.Product;
 import com.joaodarwin.course.entities.User;
 import com.joaodarwin.course.entities.enums.OrderStatus;
 import com.joaodarwin.course.repositories.CategoryRepository;
+import com.joaodarwin.course.repositories.OrderItemRepository;
 import com.joaodarwin.course.repositories.OrderRepository;
 import com.joaodarwin.course.repositories.ProductRepository;
 import com.joaodarwin.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -69,6 +74,13 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 	
