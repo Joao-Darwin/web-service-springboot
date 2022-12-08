a60c7f7db3e376b6a00257668ed5913f3b63b5ea
João Darwin <joaodarwin.ip22@gmail.com>
2022-12-08 11:13:15 -0300

    Tratamento de excecão - findById no User

--- src/main/java/com/joaodarwin/course/services/UserService.java
+++ src/main/java/com/joaodarwin/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.joaodarwin.course.entities.User;
 import com.joaodarwin.course.repositories.UserRepository;
+import com.joaodarwin.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
